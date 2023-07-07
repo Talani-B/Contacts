9675589d65bf83e622bd1e635e1a691796672e9f
Talani Balogun <Taloni5992@gmail.com>
2023-07-07 15:34:08 -0400

    initial changes

--- ContactList/src/App.jsx
+++ ContactList/src/App.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
-
+import ContactList from './components/ContactList'
 import './App.css'
 
 
-
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
